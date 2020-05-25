8f11d952a17bccfc7cf30280b531b868185db463
jackwangmeng <3290252143@qq.com>
2020-05-25 08:42:41 +0800

    测试ribbon

--- springcloud/cloud-consumer-order80/src/main/java/com/wm/springcloud/OrderMain80.java
+++ springcloud/cloud-consumer-order80/src/main/java/com/wm/springcloud/OrderMain80.java
@@ -1,13 +1,14 @@
 package com.wm.springcloud;
 
-import lombok.extern.slf4j.Slf4j;
+import com.wm.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- springcloud/cloud-provider-payment8002/src/main/java/com/wm/springcloud/controller/PaymentController.java
+++ springcloud/cloud-provider-payment8002/src/main/java/com/wm/springcloud/controller/PaymentController.java
@@ -41,4 +41,10 @@ public class PaymentController {
             return new CommonResult(444,"查询失败，没有对应记录",null);
         }
     }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB(){
+        return serverPort;
+    }
+
 }
